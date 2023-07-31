03de2e99715aa8cf6f4625c9baf07347acb620e3
Fernando Mahiler Chullo Mamani <fernando.mamani@thebridge.social>
2023-07-31 01:57:46 -0500

    feat: add firebase sdk integration

--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SendInBlueEmailService } from './services/send-in-blue-email.service';
-import {
-  FcmPushNotificationService,
-  GoogleJWTToken,
-} from './services/fcm-push-notification.service';
+// import {
+//   FcmPushNotificationService,
+//   GoogleJWTToken,
+// } from './services/fcm-push-notification.service';
+
+import admin, { ServiceAccount } from 'firebase-admin';
 import { PushNotificationServiceToken } from '../domain/push-notifications/interfaces/push-notification-service.interface';
 import { EmailServiceToken } from '../domain/email/interfaces/email-service.interface';
 import { EmailProviderOptions } from './configuration/options/email-provider-options';
@@ -13,14 +15,26 @@ import {
   TransactionalEmailsApi,
   TransactionalEmailsApiApiKeys,
 } from '@sendinblue/client';
-import { JWT } from 'google-auth-library';
+// import { JWT } from 'google-auth-library';
+import {
+  FcmSdkPushNotificationService,
+  GoogleMessagingToken,
+} from './services/fcm-sdk-push-notification.service';
 
-const services = [SendInBlueEmailService, FcmPushNotificationService];
+// const services = [SendInBlueEmailService, FcmPushNotificationService];
+const services = [SendInBlueEmailService, FcmSdkPushNotificationService];
+// const dependencyInversionServices = [
+//   { provide: EmailServiceToken, useExisting: SendInBlueEmailService },
+//   {
+//     provide: PushNotificationServiceToken,
+//     useExisting: FcmPushNotificationService,
+//   },
+// ];
 const dependencyInversionServices = [
   { provide: EmailServiceToken, useExisting: SendInBlueEmailService },
   {
     provide: PushNotificationServiceToken,
-    useExisting: FcmPushNotificationService,
+    useExisting: FcmSdkPushNotificationService,
   },
 ];
 
@@ -28,16 +42,25 @@ const dependencyInversionServices = [
   providers: [
     ...services,
     ...dependencyInversionServices,
+    // {
+    //   provide: GoogleJWTToken,
+    //   useFactory: () => {
+    //     return new JWT(
+    //       serviceAccount.client_email,
+    //       null,
+    //       serviceAccount.private_key,
+    //       [FcmPushNotificationService.MESSAGING_SCOPE],
+    //       null,
+    //     );
+    //   },
+    // },
     {
-      provide: GoogleJWTToken,
+      provide: GoogleMessagingToken,
       useFactory: () => {
-        return new JWT(
-          serviceAccount.client_email,
-          null,
-          serviceAccount.private_key,
-          [FcmPushNotificationService.MESSAGING_SCOPE],
-          null,
-        );
+        admin.initializeApp({
+          credential: admin.credential.cert(serviceAccount as ServiceAccount),
+        });
+        return admin.messaging();
       },
     },
     {
